dff6199401b0ac6084280ca130e3000df7194012
jessy.zheng@pwc.com <jessy.zheng@pwc.com>
2016-07-08 14:25:11 +0800

    add a example of how to use string template

---
+++ string/string_template.js
@@ -0,0 +1,8 @@
+// var express = require('../node_modules/express')
+// var app = express()
+//
+// app
+//   .get('/string_template.htm', (req, res) => {
+//     res.sendFile(__dirname + '/' + 'string_template.htm')
+//   })
+//   .listen(8090)
