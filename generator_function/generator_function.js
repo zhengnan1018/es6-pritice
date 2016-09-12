120b04f16bc38c9631f7f8936eab2d17212eddfb
teal-front <teal-front@users.noreply.github.com>
2016-09-12 10:56:18 +0800

    修正打印信息
    #18打印信息写错了，应该是2。因为#17行已经调用过一次next方法了。

--- generator_function/generator_function.js
+++ generator_function/generator_function.js
@@ -15,8 +15,8 @@ function* idMaker() {
 var gen = idMaker()
 console.log(gen.next().value); // 0
 console.log(gen.next().done); //false
-console.log(gen.next().value); // 1
 console.log(gen.next().value); // 2
+console.log(gen.next().value); // undefined
 console.log(gen.next().done); // true
 
 function* anotherGenerator(i) {
